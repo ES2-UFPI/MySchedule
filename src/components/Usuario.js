1cc0210548e3502138508fbc2ba4240f6de6d863
Guilherme Ferreira <guilherme199712@hotmail.com>
2019-04-19 15:10:31 -0300

    Prototipo da classe usuario.

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,6 +1,6 @@
 export default class Usuario {
-    nome: String; 
-    email: String;
+    nome: String;
+    email:String;
     idade: String;
     //Adicionar outros atributos, pq eu não sei quais precisamos agora.
     constructor(String nome, String email, String idade){
