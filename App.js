7e76ce59ff437202202cda125e989fa403ca8132
joao <joaomarcus13@hotmail.com>
2019-04-30 14:11:04 -0300

    joaomarcus#22

--- App.js
+++ App.js
@@ -5,11 +5,15 @@ import { createStackNavigator, createAppContainer } from "react-navigation";
 import Calendario from './src/components/Calendario';
 import Dias from './src/components/dias';
 import Semanas from './src/components/semanas';
+import CadastroAtividade from './src/screens/TelaCadastroAtividade'
+
 
 const navigator = createStackNavigator({
-  home: Dias,
+  home: CadastroAtividade,
   calendario: Calendario,
-  semanas: Semanas 
+  semanas: Semanas,
+  dias: Dias
+  
 })
 const App = createAppContainer(navigator);
 
