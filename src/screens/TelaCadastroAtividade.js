816ec6343ca8f4d6fc04871a98c1415e8df0bab1
joao <joaomarcus13@hotmail.com>
2019-04-30 13:36:52 -0300

    joaomarcos#22

--- src/screens/TelaCadastroAtividade.js
+++ src/screens/TelaCadastroAtividade.js
@@ -68,7 +68,7 @@ export class Prioridade extends React.PureComponent {
   
   constructor(props) {
     super(props);
-    this.state = { texto:'Prioridade' };
+    this.state = { texto:'Dificuldade' };
   }
   alterarTexto=(a)=>{
     this.setState({texto:a},this.hideMenu)
