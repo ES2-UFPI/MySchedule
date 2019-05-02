3a4456f9dd357c5e3a55ab9cf8cf580c52efc2cd
Lucas <lucasrodrigues123@hotmail.com.br>
2019-05-02 13:10:04 -0300

    Criar tela de login #36

---
+++ src/screens/TelaLogin.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { StyleSheet, Dimensions, TouchableOpacity, TextInput, View, Text, Image } from 'react-native';
+
+import logo from '../imgs/logo.png';
+
+const { width: WIDTH } = Dimensions.get('window')
+
+export default class TelaLogin extends Component 
+{
+	render ()
+	{
+		return (
+            <View style = {Styles.container}>
+                <View style = {Styles.logoContainer}>
+                    <Image source = {logo}/>
+                    <Text style = {Styles.subtitleContainer}>Tudo ao seu tempo!</Text>
+                    <View style = {Styles.formContainer}>
+                    <TextInput
+                        style = {Styles.input}
+                        keyboardType = 'email-address'
+                        placeholder = 'Endereço de Email'
+                        placeholderTextColor = '#98a7d3'
+                        returnKeyType = 'next'
+                        autoCapitalize = 'none'
+                        autoCorrect = {false}
+                        onSubmitEditing = {() => this.passwordInput.focus()}
+                    />
+                    <TextInput
+                        style = {Styles.input}
+                        placeholder = 'Senha'
+                        placeholderTextColor = '#98a7d3'
+                        secureTextEntry
+                        returnKeyType = 'go'
+                        ref = {(input) => this.passwordInput = input}
+                    />
+                    <TouchableOpacity style={Styles.button}>
+                        <Text style={Styles.buttonText}>Entrar</Text>
+                    </TouchableOpacity>
+                    </View>
+                    <Text style={Styles.footer}>MySchedule © UFPI, 2019</Text>
+                </View>
+            </View>
+		)
+	}
+}
+
+const Styles = StyleSheet.create({
+    container: 
+    {
+        flex: 1
+    },
+    logoContainer:
+    {
+        alignItems: 'center',
+        flexGrow: 1,
+        justifyContent: 'center'
+    },
+    subtitleContainer:
+    {
+        color: '#21409a',
+        marginTop: 20
+    },
+    input:
+    {
+        textAlign: 'center',
+        borderRadius: 25,
+        fontSize: 16,
+        marginTop: 15,
+        elevation: 1
+    },
+    formContainer:
+    {
+        width: WIDTH - 100,
+        marginTop: 50
+    },
+    button:
+    {
+        alignItems: 'center',
+        backgroundColor: '#21409a',
+        borderRadius: 25,
+        height: 45,
+        marginTop: 12.5,
+    },
+    buttonText:
+    {
+        color: '#fff',
+        fontSize: 16,
+        marginTop: 10
+    },
+    footer:
+    {
+        marginTop: 100, 
+        fontSize: 10, 
+        color: '#21409a'
+    }
+})
