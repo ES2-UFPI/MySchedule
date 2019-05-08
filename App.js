82c36b94c63d5d21d24d91cb16ee1698b7e500ac
Goncalo <g.neto13@hotmail.com>
2019-05-08 15:58:03 -0300

    mudei a navegação goncalo#30

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { createStackNavigator, createAppContainer, createDrawerNavigator,DrawerNavigator } from "react-navigation";
 
 import Calendario from './src/components/Calendario';
 import Dias from './src/components/dias';
@@ -8,16 +8,61 @@ import Semanas from './src/components/semanas';
 import CadastroAtividade from './src/screens/TelaCadastroAtividade'
 import ListaAtividade from './src/components/ListaAtividades'
 
+/*
+const Routes = createAppContainer(
+  createDrawerNavigator({
+    Home: ListaAtividade,
+    About: Dias,
+  })
+);
 
-const navigator = createStackNavigator({
-  home: CadastroAtividade,
+export default Routes;
+*/
+
+
+/*
+
+const Drawer = DrawerNavigator({
+  HomeDrawer: {
+    screen: ListaAtividade
+  },
+  SettingsDrawer:{
+    screen: Dias
+  }
+});
+const App = StackNavigator({
+  Drawer: {
+    screen: Drawer,
+  },
+  Home: {
+    screen: ListaAtividade
+  },
+  Settings: {
+    screen: Dias
+  },
+  View: { screen: ListaAtividade},
+})
+
+export default App;
+
+*/
+
+
+
+
+
+
+
+
+const navigator = createDrawerNavigator({
+  home: ListaAtividade,
   calendario: Calendario,
   semanas: Semanas,
   dias: Dias,
-  atividades: ListaAtividade
+  atividades: ListaAtividade,
+  cadastroDeAtividade: CadastroAtividade
   
 })
 const App = createAppContainer(navigator);
 
 export default App;
-
--- src/screens/TelaCadastroAtividade.js
+++ src/screens/TelaCadastroAtividade.js
@@ -1,5 +1,5 @@
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
@@ -10,35 +10,35 @@ import {
   Button,
   TouchableOpacity
 
-  } from 'react-native';
+} from 'react-native';
 
 import Menu, { MenuItem, MenuDivider } from 'react-native-material-menu'
 import Imagem from '../imgs/office.jpg'
 import DateTimePicker from "react-native-modal-datetime-picker";
 
-export  class DateTimePickerTester extends Component {
+export class DateTimePickerTester extends Component {
   state = {
     isDateTimePickerVisible: false
   }
- 
+
   showDateTimePicker = () => {
     this.setState({ isDateTimePickerVisible: true });
   };
- 
+
   hideDateTimePicker = () => {
     this.setState({ isDateTimePickerVisible: false });
   };
- 
+
   handleDatePicked = date => {
     console.log("A date has been picked: ", date);
     this.hideDateTimePicker();
   };
- 
+
   render() {
     return (
       <>
         <Button title="Selecionar Data e hora" onPress={this.showDateTimePicker} />
-        <DateTimePicker 
+        <DateTimePicker
           isVisible={this.state.isDateTimePickerVisible}
           onConfirm={this.handleDatePicked}
           onCancel={this.hideDateTimePicker}
@@ -65,177 +65,172 @@ export class Prioridade extends React.PureComponent {
     this._menu.show();
   };
 
-  
+
   constructor(props) {
     super(props);
-    this.state = { texto:'Dificuldade' };
+    this.state = { texto: 'Dificuldade' };
   }
-  alterarTexto=(a)=>{
-    this.setState({texto:a},this.hideMenu)
-    
-}
+  alterarTexto = (a) => {
+    this.setState({ texto: a }, this.hideMenu)
+
+  }
 
   render() {
     return (
-     
+
       <View style={styles.opcao} >
-        <Menu 
+        <Menu
           ref={this.setMenuRef}
-          button={<Text style={{fontSize:25}}onPress={this.showMenu}>{this.state.texto}</Text>}
+          button={<Text style={{ fontSize: 25 }} onPress={this.showMenu}>{this.state.texto}</Text>}
         >
-          
-          <MenuItem  onPress={()=>this.alterarTexto('Fácil')} >Fácil</MenuItem>
-          <MenuItem onPress={()=>this.alterarTexto('Médio')} >Médio</MenuItem>
-          <MenuItem onPress={()=>this.alterarTexto('Difícil')} >Difícil</MenuItem>
-         
-          
+
+          <MenuItem onPress={() => this.alterarTexto('Fácil')} >Fácil</MenuItem>
+          <MenuItem onPress={() => this.alterarTexto('Médio')} >Médio</MenuItem>
+          <MenuItem onPress={() => this.alterarTexto('Difícil')} >Difícil</MenuItem>
+
+
         </Menu>
       </View>
     );
   }
 }
-    
+
 export class Frequencia extends React.PureComponent {
-      _menu = null;
-    
-      setMenuRef = ref => {
-        this._menu = ref;
-      };
-    
-      hideMenu = () => {
-        this._menu.hide();
-      };
-    
-      showMenu = () => {
-        this._menu.show();
-      };
+  _menu = null;
 
-      
-      constructor(props) {
-        super(props);
-        this.state = { texto:'Frequencia' };
-      }
-      alterarTexto=(a)=>{
-        this.setState({texto:a},this.hideMenu)
-        
-    }
+  setMenuRef = ref => {
+    this._menu = ref;
+  };
 
-      render() {
-        return (
-         
-          <View style={styles.opcao} >
-            <Menu 
-              ref={this.setMenuRef}
-              button={<Text style={{fontSize:25}}onPress={this.showMenu}>{this.state.texto}</Text>}
-            >
-              
-              <MenuItem  onPress={()=>this.alterarTexto('Diario')} >Diario</MenuItem>
-              <MenuItem onPress={()=>this.alterarTexto('Semanal')}  >Semanal</MenuItem>
-              <MenuItem onPress={()=>this.alterarTexto('Mensal')} >Mensal</MenuItem>
-              {/*<MenuDivider />*/}
-              <MenuItem  onPress={()=>this.alterarTexto('Anual')} >Anual</MenuItem>
-              <MenuItem  onPress={()=>this.alterarTexto('Não se repete')} >Nao se repete</MenuItem>
-            </Menu>
-          </View>
-        );
-      }
-    }
-    
-  
-export default class CadastroAtividade extends Component{
-  
+  hideMenu = () => {
+    this._menu.hide();
+  };
+
+  showMenu = () => {
+    this._menu.show();
+  };
+
+
+  constructor(props) {
+    super(props);
+    this.state = { texto: 'Frequencia' };
+  }
+  alterarTexto = (a) => {
+    this.setState({ texto: a }, this.hideMenu)
+
+  }
+
   render() {
     return (
-      
-        <View style={styles.container}>
 
-        <ImageBackground
-            source={Imagem}
-            style={styles.background}>
-            <Text style={styles.texto1}>Nova Atividade</Text>
-        </ImageBackground>
+      <View style={styles.opcao} >
+        <Menu
+          ref={this.setMenuRef}
+          button={<Text style={{ fontSize: 25 }} onPress={this.showMenu}>{this.state.texto}</Text>}
+        >
 
-        <View style={styles.tela}>
-        <TextInput style={styles.input} 
-                    placeholder="Descricão" 
-                    />
-        {/*<Text style={styles.input}>Dias da semana</Text>*/}
-
-        <View style={styles.input}  >
-              <Frequencia></Frequencia>
-              <Prioridade></Prioridade>
-        </View>  
-
-
-          
-        <View style={styles.telabotao}>
-
-      
-          <DateTimePickerTester></DateTimePickerTester>
-         
-
-          </View>
-        
-        <Button  onPress={() => this.props.navigation.navigate('atividades')} title="Semanas"></Button> 
-
-        </View>
-        
-        </View>
-      
-        
+          <MenuItem onPress={() => this.alterarTexto('Diario')} >Diario</MenuItem>
+          <MenuItem onPress={() => this.alterarTexto('Semanal')}  >Semanal</MenuItem>
+          <MenuItem onPress={() => this.alterarTexto('Mensal')} >Mensal</MenuItem>
+          {/*<MenuDivider />*/}
+          <MenuItem onPress={() => this.alterarTexto('Anual')} >Anual</MenuItem>
+          <MenuItem onPress={() => this.alterarTexto('Não se repete')} >Nao se repete</MenuItem>
+        </Menu>
+      </View>
     );
   }
 }
 
+export default class CadastroAtividade extends Component {
+
+  render() {
+    return (
+
+      <View style={styles.container}>
+
+        <ImageBackground
+          source={Imagem}
+          style={styles.background}>
+          <Text style={styles.texto1}>Nova Atividade</Text>
+        </ImageBackground>
+
+        <View style={styles.tela}>
+          <TextInput style={styles.input}
+            placeholder="Descricão"
+          />
+          {/*<Text style={styles.input}>Dias da semana</Text>*/}
+
+          <View style={styles.input}  >
+            <Frequencia></Frequencia>
+            <Prioridade></Prioridade>
+          </View>
+
+
+
+          <View style={styles.telabotao}>
+
+
+            <DateTimePickerTester></DateTimePickerTester>
+
+
+          </View>
+
+        </View>
+
+      </View>
+
+
+    );
+  }
+}
+
+
 const styles = StyleSheet.create({
-  container:{
-      flex:1,
-      
+  container: {
+    flex: 1,
   },
-  background:{
-      flex:2,
-      alignItems:'flex-start',
-      justifyContent:'center',
+  background: {
+    flex: 2,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
   },
-  tela:{
-    flex:8 
+  tela: {
+    flex: 8
   },
-  
-  texto1:{
-    fontSize:35,
-    textAlign:'center',
-    marginVertical:15,
-    color:'#000000',
-   
+
+  texto1: {
+    fontSize: 35,
+    textAlign: 'center',
+    marginVertical: 15,
+    color: '#000000',
+
 
   },
-  input:{
-    
+  input: {
+
     fontSize: 25,
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-around',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
     marginVertical: 15,
     //padding:20,
     //height:50,
     //marginHorizontal:10,
-    width:'100%'
+    width: '100%'
   },
-  opcao:{
-    
-    borderColor:'black',
-    alignItems:'flex-start',
-    marginHorizontal:10,
-    justifyContent:'center',
-},
-telabotao:{
-   
-   
-    marginVertical:20,
-    marginHorizontal:10
-},
+  opcao: {
 
-  
+    borderColor: 'black',
+    alignItems: 'flex-start',
+    marginHorizontal: 10,
+    justifyContent: 'center',
+  },
+  telabotao: {
+    marginVertical: 20,
+    marginHorizontal: 10
+  }
 
- 
+
+
+
 });
