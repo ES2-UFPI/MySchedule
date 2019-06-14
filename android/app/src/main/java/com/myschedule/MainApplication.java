5896b12c0f02378dc2897117e219ceeea069cd25
joao <joaomarcus13@hotmail.com>
2019-06-14 18:09:00 -0300

    Alerta de notificacao joaomarcos#81

--- android/app/src/main/java/com/myschedule/MainApplication.java
+++ android/app/src/main/java/com/myschedule/MainApplication.java
@@ -3,6 +3,7 @@ package com.myschedule;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage; 
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new RNFirebasePackage(),
             new VectorIconsPackage(),
             new AsyncStoragePackage(),
